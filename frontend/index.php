50b8fe6dbe73274a6c43ad7f4d77f6459538b246
Armand Sumo <armandsumo@gmail.com>
2022-03-25 12:15:10 +0100

    Update index.php

--- frontend/index.php
+++ frontend/index.php
@@ -1,3 +1,16 @@
 <!--
     hey
 -->
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset='utf-8'>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
+    <title>Index</title>
+    <link rel="shortcut icon" href="">
+</head>
+<body>
+    
+</body>
