f6a86aeab7b168e17cb266d10dbbfe7505c920f8
Armand Sumo <armandsumo@gmail.com>
2022-03-28 17:58:16 +0200

    change from mysqli to PDO

--- backend/config.php
+++ backend/config.php
@@ -4,9 +4,20 @@ $username = 'root';
 $password = '' ;
 $dbname = 'imm';
 
-$conn = new mysqli($host, $username,$password,$dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// $conn = new mysqli($host, $username,$password,$dbname);
+// // Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+// //ensure utf8 character encoding
+// mysqli_set_charset($conn, "utf8");
+
+try {
+  $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+  $conn->exec("set names utf8mb4");
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  //echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
 }
-mysqli_set_charset($conn, "utf8");
