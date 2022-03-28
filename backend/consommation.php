95dc6fbf2efa7ace1cabff0e7f12c6ae4e563054
Armand Sumo <armandsumo@gmail.com>
2022-03-28 23:59:11 +0200

    added 'consommation' selection

--- backend/consommation.php
+++ backend/consommation.php
@@ -1,2 +1,13 @@
 <?php
-include 'config.php';
+require('config.php');
+
+function get_conso($conn){
+    //récupère les aliments
+    $stmt = $conn->prepare("SELECT nom FROM aliment INNER JOIN consommation ON aliment.id_ali = consommation.id_ali");
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
+}
+
+$result = get_conso($conn);
+exit(json_encode($result));
