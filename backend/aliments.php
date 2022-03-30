636103eb55f32357e945d5a961ed57e002765c0a
Armand Sumo <armandsumo@gmail.com>
2022-03-30 13:15:20 +0200

    fixed type query

--- backend/aliments.php
+++ backend/aliments.php
@@ -3,14 +3,14 @@ require('config.php');
 
 function get_aliments($conn){
   //récupère les aliments
-  $stmt = $conn->prepare("SELECT aliment.nom FROM aliment");
+  $stmt = $conn->prepare("SELECT DISTINCT aliment.nom FROM aliment");
   $stmt->execute();
   $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
   return $result;
   }
 function get_aliments_type($conn){
   // récupère les aliments d'un type choisi
-  $stmt = $conn->prepare("SELECT aliment.nom FROM aliment WHERE aliment.id_type= :id_type");
+  $stmt = $conn->prepare("SELECT DISTINCT aliment.type FROM aliment WHERE aliment.id_type= :id_type");
   $id_type = $_GET['id_type'];
   echo($id_type);
   $stmt->bindParam(':id_type', $id_type);
@@ -19,12 +19,12 @@ function get_aliments_type($conn){
 }
 function get_aliments_type_bs($conn){
   // récupère les aliments d'un type choisi
-  $stmt = $conn->prepare("SELECT aliment.nom FROM aliment where type LIKE CONCAT('%', :typename, '%')");
+  $stmt = $conn->prepare("SELECT DISTINCT aliment.type FROM aliment where type LIKE CONCAT('%', :typename, '%')");
   $type =  $_GET['type'];
   $stmt->bindValue(':typename', $type, PDO::PARAM_STR);
   $stmt->execute();
   while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    $data[] = $row['nom'];
+    $data[] = $row['type'];
   }
   return $data;
 }
