0e583ee88448869b460432351d5c185d458951b4
Armand Sumo <armandsumo@gmail.com>
2022-04-01 18:37:01 +0200

    begin 'nouveau' page setup

---
+++ frontend/nouveau.php
@@ -0,0 +1,12 @@
+    </div>
+        <div class="row">  
+            <div class="col-md-12 text-left">   
+                <div> Sélectionnez un type</div>     
+                <input class="typeahead form-control" style="width:300px;" type="text">  
+            </div>  
+        </div>  
+    </table>
+</body>
+</html>
+<?php require_once('template_footer.php');
+?>
