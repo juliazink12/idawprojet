1889ce82cec754230e7dd10f430698963c1144a3
Armand Sumo <armandsumo@gmail.com>
2022-04-01 02:20:17 +0200

    setup files for CRUD

--- backend/consommation.php
+++ backend/consommation.php
@@ -3,36 +3,36 @@ require('config.php');
 
 function get_conso($conn){
     //get 'consommation' over a set time interval
-    $sql = "SELECT nom as 'nom',date as 'date' FROM aliment INNER JOIN consommation ON aliment.id_ali = consommation.id_ali";
+    $sql = "SELECT nom as 'nom',date as 'date',quantite as 'quantite' FROM aliment INNER JOIN consommation ON aliment.id_ali = consommation.id_ali";
     if(isset($_POST['span'])){
         $span = $_POST['span'];
         switch($span){
             case 'day':
                 //today
-                $sql = "SELECT nom,date FROM aliment INNER JOIN consommation ON aliment.id_ali = consommation.id_ali
+                $sql = "SELECT nom,date,quantite FROM aliment INNER JOIN consommation ON aliment.id_ali = consommation.id_ali
                         WHERE date >= DATE_ADD(CURDATE(), INTERVAL 0 DAY) 
                         and   date <= CURDATE()";
                 break;
             case 'week':
             //past week
-                $sql = "SELECT nom,date FROM aliment INNER JOIN consommation ON aliment.id_ali = consommation.id_ali
+                $sql = "SELECT nom,date,quantite FROM aliment INNER JOIN consommation ON aliment.id_ali = consommation.id_ali
                         WHERE date >= DATE_ADD(CURDATE(), INTERVAL -7 DAY) 
                         and   date <= CURDATE()";
                 break;
             case 'month':
             //past month
-                $sql = "SELECT nom,date FROM aliment INNER JOIN consommation ON aliment.id_ali = consommation.id_ali
+                $sql = "SELECT nom,date,quantite FROM aliment INNER JOIN consommation ON aliment.id_ali = consommation.id_ali
                         WHERE date >= DATE_ADD(CURDATE(), INTERVAL -31 DAY) 
                         and   date <= CURDATE()";
                 break;
             case 'year';
             //past year 
-                $sql = "SELECT nom,date FROM aliment INNER JOIN consommation ON aliment.id_ali = consommation.id_ali
+                $sql = "SELECT nom,date,quantite FROM aliment INNER JOIN consommation ON aliment.id_ali = consommation.id_ali
                         WHERE date >= DATE_ADD(CURDATE(), INTERVAL -1 YEAR) 
                         and   date <= CURDATE()";
                 break;
             case 'all';
-                $sql =  "SELECT nom,date FROM aliment INNER JOIN consommation ON aliment.id_ali = consommation.id_ali";
+                $sql =  "SELECT nom,date,quantite FROM aliment INNER JOIN consommation ON aliment.id_ali = consommation.id_ali";
             //all
                 break;
         }
@@ -42,6 +42,31 @@ function get_conso($conn){
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
     return $result;
 }
-
+function add_conso($conn){
+    $result = $conn;
+    return $result;
+}
+function remove_conso($conn){
+    $result = $conn;
+    return $result;
+}
+function edit_conso($conn){
+    $result = $conn;
+    return $result;
+}
+// switch($_SERVER["REQUEST_METHOD"]) {
+//     case 'GET':
+//         $result = get_conso($conn);
+//         break;
+//     case 'POST':
+//         $result = add_conso($conn);
+//         break;
+//     case 'DELETE':
+//         $result = remove_conso($conn);
+//         break;
+//     case 'PUT':
+//         $result = edit_conso($conn);
+//         break;       
+// }
 $result = get_conso($conn);
 exit(json_encode($result));
--- frontend/index.php
+++ frontend/index.php
@@ -9,14 +9,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="shortcut icon" href="">
     <!-- Import jquery before bootstrap-typehead -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">  
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"> 
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>  
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.1/bootstrap3-typeahead.min.js"></script>
     <!-- Import Datatables after JQuery-->
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.11.5/date-1.1.2/r-2.2.9/sc-2.0.5/sb-1.3.2/sp-2.0.0/sl-1.3.4/datatables.min.css"/>
-    <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.11.5/date-1.1.2/r-2.2.9/sc-2.0.5/sb-1.3.2/sp-2.0.0/sl-1.3.4/datatables.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.11.5/b-2.2.2/date-1.1.2/r-2.2.9/sl-1.3.4/datatables.min.css"/>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.11.5/b-2.2.2/date-1.1.2/r-2.2.9/sl-1.3.4/datatables.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.2/css/buttons.dataTables.css" />
+    <script src="https://cdn.datatables.net/buttons/1.6.2/js/dataTables.buttons.js" ></script>
     <script  src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script> 
     <script  src="https://cdn.datatables.net/plug-ins/1.11.5/dataRender/datetime.js"></script>
+    <script src="js/dataTables.altEditor.free.js" ></script>
      
     <title>Index</title>   
 </head>
@@ -31,15 +34,22 @@
             <td><input type="text" id="max" name="max"></td>
         </tr>
     </tbody>
-        <table id="example" class="display" style="width:100%">
-
+        <div class="table-responsive">
+        <table id="conso" class="table table-bordered table-striped">
+        <div align="center">
+        <button type="button" id="add_button" data-toggle="modal" data-target="#consoModal" class="btn btn-info btn-lg">Ajouter</button>
+        </div>
             <thead>
                 <tr>
-                    <th>Nom</th>
-                    <th>Date</th>
+                    <th width="30%">Nom</th>
+                    <th width="30%">Date</th>
+                    <th width="30%">Quantité</th>
+                    <th width="5%">Editer</th>
+                    <th width="5%">Effacer</th>
                 </tr>
             </thead>
         </table>
+        </div>
         <div class="row">  
             <div class="col-md-12 text-left">   
                 <div> Sélectionnez un type</div>     
@@ -47,8 +57,58 @@
             </div>  
         </div>  
     </table>
-<script>
-
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-primary" href="login.html">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+<div id="consoModal" class="modal fade">
+ <div class="modal-dialog">
+  <form method="post" id="conso_form" enctype="multipart/form-data">
+   <div class="modal-content">
+    <div class="modal-header">
+     <button type="button" class="close" data-dismiss="modal">&times;</button>
+     <h4 class="modal-title">Ajouter une consommation</h4>
+    </div>
+    <div class="modal-body">
+    <!-- C'est ici qu'il faut insérer le typehead de sélection des aliments,ainsi que la logique de restriction par type-->
+     <label>Nom de la consommation</label>
+     <input type="text" name="nom" id="nom" class="form-control" />
+     <br />
+     <label>Date</label>
+     <input type="date" name="date" id="date" class="form-control" />
+     <br />
+     <label>Quantité</label>
+     <input type="text" name="quantite" id="quantite" class="form-control" />
+     <br />
+    </div>
+    <div class="modal-footer">
+     <input type="hidden" name="id_conso" id="id_conso" />
+     <input type="hidden" name="operation" id="operation" />
+     <input type="submit" name="action" id="action" class="btn btn-success" value="Add" />
+     <button type="button" class="btn btn-default" data-dismiss="modal">Fermer</button>
+    </div>
+   </div>
+  </form>
+ </div>
+</div>
+</body>
+</html>
+<script type="text/javascript" language="javascript">
     var minDate, maxDate;
  
     // Custom filtering function which will search data in column four between two values
@@ -70,17 +130,23 @@
             return false;
         }
     );
-    // _span : globall
     var _span = 'all';
-    function onFormSubmit() {
-        event.preventDefault();
-        // Test de sélection du type d'aliment
-        var set_type = 1;
-        var id_type = 0;
-        var type = { SET_TYPE: set_type, ID_TYPE: id_type };
-        ajaxSendIDType(type);
-    }
+    // function onFormSubmit() {
+    //     event.preventDefault();
+    //     // Test de sélection du type d'aliment
+    //     var set_type = 1;
+    //     var id_type = 0;
+    //     var type = { SET_TYPE: set_type, ID_TYPE: id_type };
+    //     ajaxSendIDType(type);
+    // }
     $(document).ready(function() {
+        // Add Consommation
+        $('#add_button').click(function(){
+        $('#conso_form')[0].reset();
+        $('.modal-title').text("Ajouter une Consommation");
+        $('#action').val("Add");
+        $('#operation').val("Add");
+        });
         //Select the type of food item
         $('input.typeahead').typeahead({  
             source:  function (query, process) {  
@@ -103,7 +169,7 @@
             format: 'MMMM Do YYYY'
         });
         //Define 'Consommation' table
-        var table_conso = $('#example').DataTable( {
+        var table_conso = $('#conso').DataTable( {
             "processing": true,
             //"serverSide": true,
             "ajax": {
@@ -116,7 +182,20 @@
             },
             "columns": [
                 { data : "nom" },
-                { data : "date"}
+                { data : "date"},
+                { data : "quantite"},
+                {
+                    data: null,
+                    className: "dt-center editor-edit",
+                    defaultContent: '<i class="fa fa-pencil"/>',
+                    orderable: false
+                },
+                {
+                    data: null,
+                    className: "dt-center editor-delete",
+                    defaultContent: '<i class="fa fa-trash"/>',
+                    orderable: false
+                }
             ]
         });
         // Refilter the table
@@ -126,10 +205,77 @@
 
         
     } );
-
+    $(document).on('submit', '#conso_form', function(event){
+    event.preventDefault();
+    var Nom = $('#nom').val();
+    var Date = $('#date').val();
+    var Quantite = $('#quantite').val();
+    if(Nom != '' && Date != '' && Quantite != '')
+    {
+    $.ajax({
+        url:"../backend/consommation.php",
+        method:'POST',
+        data:new FormData(this),
+        contentType:false,
+        processData:false,
+        success:function(data)
+        {
+        alert(data);
+        $('#conso_form')[0].reset();
+        $('#consoModal').modal('hide');
+        table_conso.ajax.reload();
+        }
+    });
+    }
+    else
+    {
+    alert("Les trois champs sont requis.");
+    }
+    });
+    
+    $(document).on('click', '.update', function(){
+    var conso_id = $(this).attr("id");
+    $.ajax({
+    url:"../backend/consommation.php",
+    method:"PUT",
+    data:{id_conso:id_conso},
+    dataType:"json",
+    success:function(data)
+    {
+        $('#consoModal').modal('show');
+        $('#nom').val(data.nom);
+        $('#date').val(data.date);
+        $('#quantite').val(data.quantite);
+        $('.modal-title').text("Editer la Consommation");
+        $('#id_conso').val(id_conso);
+        $('#action').val("Edit");
+        $('#operation').val("Edit");
+    }
+    })
+    });
+    $(document).on('click', '.delete', function(){
+    var id_conso = $(this).attr("id");
+    if(confirm("Are you sure you want to delete this?"))
+    {
+    $.ajax({
+        url:"../backend/consommation.php",
+        method:"DELETE",
+        data:{id_conso:id_conso},
+        success:function(data)
+        {
+        alert(data);
+        table_conso.ajax.reload();
+        }
+    });
+    }
+    else
+    {
+    return false; 
+    }
+    });
     function SendTimeSpan(btn){
         _span = $(btn).val();
-        $('#example').DataTable().ajax.reload();               
+        $('#conso').DataTable().ajax.reload();               
     }
     function ajaxGETAliments(){  
         $.ajax({
@@ -229,3 +375,4 @@
     // }   
     </script>   
 </body>
+</html>
