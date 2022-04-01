1dabe4879f132ea8c0c73772e6ecea0143cf40ed
Armand Sumo <armandsumo@gmail.com>
2022-04-02 00:31:08 +0200

    added typehead

--- frontend/nouveau.php
+++ frontend/nouveau.php
@@ -1,12 +1,59 @@
-    </div>
-        <div class="row">  
-            <div class="col-md-12 text-left">   
-                <div> Sélectionnez un type</div>     
-                <input class="typeahead form-control" style="width:300px;" type="text">  
-            </div>  
+<!-- Ajouter historique des consommations -->
+<?php require_once('template_header.php');?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset='utf-8'>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="">
+    <!-- Import jquery before bootstrap-typehead -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.1/bootstrap3-typeahead.min.js"></script>
+    <!-- Import Datatables after JQuery-->
+    <!-- <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.11.5/b-2.2.2/date-1.1.2/r-2.2.9/sl-1.3.4/datatables.min.css"/>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.11.5/b-2.2.2/date-1.1.2/r-2.2.9/sl-1.3.4/datatables.min.js"></script> -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css"/>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/datetime/1.1.2/css/dataTables.dateTime.min.css"/>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/2.2.2/css/buttons.dataTables.min.css"/>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.dataTables.min.css"/>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/searchpanes/2.0.0/css/searchPanes.dataTables.min.css"/>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/select/1.3.4/css/select.dataTables.min.css"/>
+
+    <script type="text/javascript" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/datetime/1.1.2/js/dataTables.dateTime.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/searchpanes/2.0.0/js/dataTables.searchPanes.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/select/1.3.4/js/dataTables.select.min.js"></script>
+    <!-- Import altEditorFree before momentjs and after Datatable-->
+    <script src="js/dataTables.altEditor.free.js">
+    <script  src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
+        
+</head>
+<body>   
+    <div class="row">  
+        <div class="col-md-12 text-left">   
+            <div> Sélectionnez un type</div>     
+            <input id="inputTypeahead"class="typeahead form-control" style="width:300px;" type="text">  
         </div>  
-    </table>
+    </div>  
 </body>
 </html>
+<script>
+    $('#inputTypeahead').typeahead({  
+        source:  function (query, process) {  
+        return $.get('../backend/aliments.php', { type: query }, function (data) {  
+                console.log(data);  
+                data = $.parseJSON(data);  
+                return process(data);  
+            }); 
+        }, 
+        updater: function(item) {
+        // updater is run after user click
+            console.log(item);
+        } 
+    });
+</script>
 <?php require_once('template_footer.php');
 ?>
